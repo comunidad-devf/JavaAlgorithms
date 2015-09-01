23b242464ebe37391058b50bf2f10308efa3c494
Pablo Trinidad <pabloatp0204@gmail.com>
2015-09-01 16:46:01 -0500

    13

---
+++ 13 String Methods/StringMethods.java
@@ -0,0 +1,34 @@
+public class StringMethods {
+	public static void main(String[] args) {
+		String myText = "Hello!, my name is Pablo. Nice to meet you";
+
+		// Length method
+		// int length = myText.length();
+		// System.out.println(length);
+
+		// CharAt method
+		// char myChar = myText.charAt(0);
+		// System.out.println(myChar);
+		// for (int i = 0; i < myText.length(); i++)
+		// 	System.out.println(myText.charAt(i));
+
+		// Equals method
+		// System.out.println(myText.equals("Hallo!, mein Name ist Paul. Nice to meet"));
+		// System.out.println(myText.equals("Hello!, my name is Pablo. Nice to meet you"));
+		// if (myText.equals("Hallo!, mein Name ist Paul. Nice to meet"))
+		// 	System.out.println("The text is a greeting in german.");
+		// else
+		// 	System.out.println("The text is a greeting in english.");
+
+		// IsEmpty method
+		// System.out.println(myText.isEmpty());
+		// if (myText.isEmpty())
+		// 	System.out.println("The text is empty");
+		// else
+		// 	System.out.println("The text is not empty");
+
+		// Replace method
+		// System.out.println(myText.replace(" ", "\n"));
+
+	}
+}
